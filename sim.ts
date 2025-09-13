db69d556e57a67b5a15232f4c8fb7d6d378e1b71
Cryptid <cryptidbleh@icloud.com>
2025-09-13 23:08:37 +0200

    Create sim.ts

---
+++ sim.ts
@@ -0,0 +1,29 @@
+namespace microUtilities {
+    export function storageCapacity(unit: StorageUnit): number {
+        return 0;
+    }
+    export function storageUsage(unit: StorageUnit): number {
+        return 0;
+    }
+    export function ramCapacity(unit: StorageUnit): number {
+        return 0;
+    }
+    export function ramUsage(unit: StorageUnit): number {
+        return 0;
+    }
+    export function cpuSpeed(): number {
+        return 0;
+    }
+    export function togglePixel(x: number, y: number): void {
+        // no-op
+    }
+    export function setPixel(x: number, y: number, on: boolean): void {
+        // no-op
+    }
+    export function setPixelBrightness(x: number, y: number, brightness: number): void {
+        // no-op
+    }
+    export function setImage(img: string): void {
+        // no-op
+    }
+}
